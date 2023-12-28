acf545316ae2642d96791593a7d7480d755ebf42
Talmaciu Elena <148704214+ellat99@users.noreply.github.com>
2023-12-28 21:30:19 +0200

    Initial commit

---
+++ weather.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const apiKey = "ae51bea0c009afb0a2f9345a6ab5842c";
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+  const searchBox = document.querySelector(".search input");
+  const searchBtn = document.querySelector(".search button");
+  const weatherIcon = document.querySelector(".weather-icon");
+
+  async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status === 404) {
+      // Orașul nu a fost găsit
+      document.querySelector(".error").innerHTML = "Invalid city name";
+      document.querySelector(".error").style.display = "block";
+      document.querySelector(".weather").style.display = "none";
+    } else {
+      // Orașul a fost găsit
+      let data = await response.json();
+      console.log(data);
+
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+      if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images-weather/clouds.png";
+      } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images-weather/clear.png";
+      } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images-weather/rain.png";
+      } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images-weather/most.png";
+      } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images-weather/drizzle.png";
+      }
+
+      // Afiseaza zona cu informatii despre vreme
+      document.querySelector(".weather").style.display = "block";
+      document.querySelector(".error").style.display = "none"; // Ascunde mesajul de eroare
+    }
+  }
+
+  searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+  });
+});
